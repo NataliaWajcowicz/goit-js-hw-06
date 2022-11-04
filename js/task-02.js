1a97581a24d34fec6c41b32bb78e3a6f7d7acbf9
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-11-04 20:50:20 +0100

    Done task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector("ul#ingredients");
+console.log(list);
+
+for (const ingredient of ingredients) {  
+  const item = document.createElement("li");
+  item.textContent = ingredient;  
+  list.append(item); 
+}
+
+
