25a327bfe56536364fb33ba7ce41a870405b1fae
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-11-05 17:14:43 +0100

    done task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        console.log("Fill all fields!");
+        
+    }
+
+    const user = {
+        email: email.value,
+        password: password.value,
+    }
+
+    console.log(user);
+    event.currentTarget.reset();
+
+}
+
 
