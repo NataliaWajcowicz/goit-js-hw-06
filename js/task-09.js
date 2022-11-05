8ddfb8004edcc16f79fd3ceba966766bddd0da0e
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-11-05 17:32:31 +0100

    done task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const span= document.querySelector(".color")
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+  
 }
+
+
+const color = getRandomHexColor();
+
+
+btn.addEventListener("click", () => {
+  body.style.backgroundColor = `${color}`;
+  span.textContent = `${color}`;
+})
