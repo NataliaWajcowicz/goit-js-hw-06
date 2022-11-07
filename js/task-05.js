705df2ce3926bea1389a92c1204d3a51402988fd
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-11-07 22:53:59 +0100

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,10 +2,11 @@ const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-    output.textContent = event.currentTarget.value;
+    output.textContent = event.currentTarget.value; 
+
+    if (event.currentTarget.value==="") {
+        output.textContent = "Anonymous";
+    }
 
 });
 
-
-console.log(textInput);
-console.log(output);
