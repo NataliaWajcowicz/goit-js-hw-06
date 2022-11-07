25c061546b645011fcf8e68bd2eefc16bc7dbf7c
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-11-07 21:57:53 +0100

    update task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,27 +1,17 @@
-const list = document.body.querySelectorAll(".item");
+const list = [...document.body.querySelectorAll(".item")];
 
 console.log(`Number of categories: ${list.length}`);
 
-const categoryFirst = document.body.childNodes[3].childNodes[1].childNodes[1];
-
-const listFirst = document.body.childNodes[3].childNodes[1].childNodes[3].children;
-
-const categorySecond = document.body.childNodes[3].childNodes[3].childNodes[1];
-
-const listSecond = document.body.childNodes[3].childNodes[3].childNodes[3].children;
-
-const categoryThird = document.body.childNodes[3].childNodes[5].childNodes[1];
-
-const listThird = document.body.childNodes[3].childNodes[5].childNodes[3].children;
+const listItems = [...document.querySelectorAll("li.item")];
 
 
 
-console.log(`Category:${categoryFirst.textContent}`);
-console.log(`Elements ${listFirst.length}`);
-console.log(`Category:${categorySecond.textContent}`);
-console.log(`Elements ${listSecond.length}`);
-console.log(`Category:${categoryThird.textContent}`);
-console.log(`Elements ${listThird.length}`);
+listItems.forEach((listItem) => {
+    const title = listItem.querySelector("h2");
+    console.log(`Category: ${title.textContent}`)
+    const items = [...listItem.querySelectorAll("li")];
+    console.log(`Elements: ${ items.length }`)
+})
 
 
 
