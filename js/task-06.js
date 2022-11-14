21567960d982416e915436166366c48d7bb2f1bb
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-11-14 11:35:47 +0100

    update 2 task-06

--- js/task-06.js
+++ js/task-06.js
@@ -8,10 +8,10 @@ const maxLength = parseInt(input.getAttribute("data-length"));
 
 input.addEventListener("change", (event) => {
     const text = event.currentTarget.value;
-    input.classList.add("valid");
-    input.classList.remove("invalid")
-    if (text.length > maxLength) {
-        input.classList.add("invalid")
+    input.classList.add("invalid");
+    if (text.length === maxLength) {
+        input.classList.add("valid");
+        input.classList.remove("invalid")
     };
     
     
